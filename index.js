b76971660afe65804abc99c7cc6014f0e279660f
dafikk <dafikkece24@gmail.com>
2025-05-29 20:02:10 +0700

    sedikit perubahan pada loading page

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Animasi Untuk Loading Page nya
 $(window).on('load', function(){
     $('.spin-wrapper').fadeOut(800);
 });
 
+// Navbar Responsive
 const navLinks = document.querySelector('.nav-links')
 function onToggleMenu(e){
     e.name= e.name ==='close' ? 'menu' : 'close'
